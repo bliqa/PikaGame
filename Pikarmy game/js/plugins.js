55072ec142082459dd3819cdd15b49138f88b83b
Dragon <ios2004@icloud.com>
2020-07-22 21:42:10 +0700

    ver 0.4.2

--- Pikarmy game/js/plugins.js
+++ Pikarmy game/js/plugins.js
@@ -6,5 +6,6 @@ var $plugins =
 {"name":"AltMenuScreen","status":true,"description":"Alternative menu screen layout.","parameters":{}},
 {"name":"AltSaveScreen","status":true,"description":"Alternative save/load screen layout.","parameters":{}},
 {"name":"EnemyBook","status":true,"description":"Displays detailed statuses of enemies.","parameters":{"Unknown Data":"??????"}},
-{"name":"GALV_MessageBusts","status":true,"description":"(v.2.8) Displays a bust image instead of selected face image","parameters":{"Bust Priority":"0","Bust Position":"1","Text X Offset":"390","Fade Out Speed":"32","Filename Append":""}}
+{"name":"GALV_MessageBusts","status":true,"description":"(v.2.8) Displays a bust image instead of selected face image","parameters":{"Bust Priority":"0","Bust Position":"1","Text X Offset":"390","Fade Out Speed":"32","Filename Append":""}},
+{"name":"Kath_GameOver","status":true,"description":"Change what happens when the party dies or Game Over is called.","parameters":{"Party Death Common Event ID":"","Show Game Over Scene":"true","Reload Last Save":"false","After Game Over Common Event ID":""}}
 ];
